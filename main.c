72a1abc485bcee5185aad67d2799cf1562e49c98
JPGP_E <garciaperezjuanpablo7@gmail.com>
2022-03-05 10:46:40 -0600

    Version 0.4

--- main.c
+++ main.c
@@ -19,20 +19,10 @@
 #include "fsl_debug_console.h"
 #include "fsl_common.h"
 #include "fsl_adc16.h"
-#include "fsl_dmamux.h"
-#include "fsl_edma.h"
 
-
-#define DEMO_ADC16_CHANNEL       12U
+#define DEMO_ADC16_BASE          ADC0
 #define DEMO_ADC16_CHANNEL_GROUP 0U
-#define DEMO_ADC16_BASEADDR      ADC0
-#define DEMO_DMAMUX_BASEADDR     DMAMUX0
-#define DEMO_DMA_CHANNEL         0U
-#define DEMO_DMA_ADC_SOURCE      40U
-#define DEMO_DMA_BASEADDR        DMA0
-#define ADC16_RESULT_REG_ADDR    0x4003b010U
-#define DEMO_DMA_IRQ_ID          DMA0_IRQn
-#define DEMO_ADC16_SAMPLE_COUNT 16U /* The ADC16 sample count. */
+#define DEMO_ADC16_USER_CHANNEL  12U
 
 #define x_n_lenght (7)//
 #define h_n_lenght (7)
@@ -51,47 +41,14 @@ void volume_down(int *volume);
 
 uint8_t state_select();
 
-/*******************************************************************************
- * Prototypes
- ******************************************************************************/
-/*!
- * @brief Initialize the EDMA.
- */
-static void EDMA_Configuration(void);
-
-/*!
- * @brief Initialize the DMAMUX.
- */
-static void DMAMUX_Configuration(void);
-
-/*!
- * @brief Initialize the ADC16.
- */
-static void ADC16_Configuration(void);
-
-/*!
- * @brief Process ADC values.
- */
-static void ProcessSampleData(void);
-
-/*!
- * @brief Callback function for EDMA.
- */
-static void Edma_Callback(edma_handle_t *handle, void *userData, bool transferDone, uint32_t tcds);
-
-volatile bool g_Transfer_Done = false; /* DMA transfer completion flag. */
-uint32_t g_adc16SampleDataArray[DEMO_ADC16_SAMPLE_COUNT];
-uint32_t g_avgADCValue = 0U; /* Average ADC value .*/
-edma_handle_t g_EDMA_Handle; /* Edma handler. */
-edma_transfer_config_t g_transferConfig;
-const uint32_t g_Adc16_16bitFullRange = 65536U;
+const uint32_t g_Adc16_12bitFullRange = 4096U;
 
 int main(void) {
 
 	float h_0 [h_n_lenght] = {1, 0, 0, 0, 0, 0, 0};	//Respuesta 1 para reproducri cacnión normal
 	float h_1 [h_n_lenght] = {-0.1050, -0.1448, -0.1721, 0.8182, -0.1721, -0.1448, -0.1050};//Respuesta h1
 	float h_2 [h_n_lenght] = {0.0802, 0.0860, 0.0897, 0.0909, 0.0897, 0.0860, 0.0802};//Respuesta h2
-	float *h=&h_0;
+	float *h = &h_0;
 	float x[x_n_lenght+3] = {0};
 	float *x1 = &x, *x2 = &x[7];
 	float y;
@@ -112,17 +69,33 @@ int main(void) {
     DAC_init_config(&dacConfigStruct);
 
 
-
-    EDMA_Configuration();   /* Initialize EDMA. */
-    DMAMUX_Configuration(); /* Initialize DMAMUX. */
-    ADC16_Configuration();  /* Initialize ADC16. */
-
-    adcChnConfig.channelNumber = DEMO_ADC16_CHANNEL;
-#if defined(FSL_FEATURE_ADC16_HAS_DIFF_MODE) && FSL_FEATURE_ADC16_HAS_DIFF_MODE
-    adcChnConfig.enableDifferentialConversion = false;
+    adc16_config_t adc16ConfigStruct;
+    adc16_channel_config_t adc16ChannelConfigStruct;
+    ADC16_GetDefaultConfig(&adc16ConfigStruct);
+#ifdef BOARD_ADC_USE_ALT_VREF
+    adc16ConfigStruct.referenceVoltageSource = kADC16_ReferenceVoltageSourceValt;
 #endif
-    adcChnConfig.enableInterruptOnConversionCompleted = false;
-    ADC16_SetChannelConfig(DEMO_ADC16_BASEADDR, DEMO_ADC16_CHANNEL_GROUP, &adcChnConfig);
+    ADC16_Init(DEMO_ADC16_BASE, &adc16ConfigStruct);
+    ADC16_EnableHardwareTrigger(DEMO_ADC16_BASE, false); /* Make sure the software trigger is used. */
+#if defined(FSL_FEATURE_ADC16_HAS_CALIBRATION) && FSL_FEATURE_ADC16_HAS_CALIBRATION
+    if (kStatus_Success == ADC16_DoAutoCalibration(DEMO_ADC16_BASE))
+    {
+        PRINTF("ADC16_DoAutoCalibration() Done.\r\n");
+    }
+    else
+    {
+        PRINTF("ADC16_DoAutoCalibration() Failed.\r\n");
+    }
+#endif /* FSL_FEATURE_ADC16_HAS_CALIBRATION */
+
+    adc16ChannelConfigStruct.channelNumber                        = DEMO_ADC16_USER_CHANNEL;
+    adc16ChannelConfigStruct.enableInterruptOnConversionCompleted = false;
+#if defined(FSL_FEATURE_ADC16_HAS_DIFF_MODE) && FSL_FEATURE_ADC16_HAS_DIFF_MODE
+    adc16ChannelConfigStruct.enableDifferentialConversion = false;
+#endif /* FSL_FEATURE_ADC16_HAS_DIFF_MODE */
+
+    ADC16_SetChannelConfig(DEMO_ADC16_BASE, DEMO_ADC16_CHANNEL_GROUP, &adc16ChannelConfigStruct);
+
     red();
     PIT_delay(PIT_0, SYSTEM_CLOCK, DELAY_TIME);// EMPEZAMOS LA FREQ DE MUESTREO
 
@@ -154,14 +127,18 @@ int main(void) {
     		}
     	if(PIT_get_interrupt_flag_status()){
     		//leear del adc y agregar a read_value
+    		ADC16_SetChannelConfig(DEMO_ADC16_BASE, DEMO_ADC16_CHANNEL_GROUP, &adc16ChannelConfigStruct);
     	    for(int i=x_n_lenght;i>0;i--)	// este for recorre el arreglo y las muestras pasadas para pushear una nueva
     		{
 
     			x[i] = x[i-1];
 
     		}
-    		ProcessSampleData();
-    		x[0]= g_avgADCValue + volume;	//no se si sea el sumar o que miltiplique
+            while (0U == (kADC16_ChannelConversionDoneFlag &
+                          ADC16_GetChannelStatusFlags(DEMO_ADC16_BASE, DEMO_ADC16_CHANNEL_GROUP)))
+            {
+            }
+    		x[0]= ADC16_GetChannelConversionValue(DEMO_ADC16_BASE, DEMO_ADC16_CHANNEL_GROUP) + volume;	//no se si sea el sumar o que miltiplique
     		//void pointer_void_funct(float *x, float *h,float *y)
     		pointer_void_funct(x,h,&y);
     		conv = floor(y);
@@ -170,6 +147,7 @@ int main(void) {
     		DAC_write_value(&dacConfigStruct,conv);
     		PIT_clear_interrupt_flag();
     	}
+    }
     return 0 ;
 }
 
